02b25be156be506755ec74eff9e58b49fdf193eb
Daisy Jane Batislaong <71486565+DaisyBatislaong@users.noreply.github.com>
2021-03-03 22:35:39 +0800

    change router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,11 @@
 import React from "react";
-import About from "../components/About";
 import MainSection from "../components/MainSection";
-import Projects from "../components/Projects";
-import { Router, Link } from "@reach/router";
 
 const index = () => {
    return (
-  
-         <Router>
-            <MainSection path="/" />
-            <About path="/about" />
-            <Projects path="/projects"/>
-         </Router>
+      <React.Fragment>
+         <MainSection />
+      </React.Fragment>
    );
 };
 
