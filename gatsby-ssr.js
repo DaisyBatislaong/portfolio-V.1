1c8a47c1558487fb0863043a4797132ab8bb4fca
Daisy Jane Batislaong <71486565+DaisyBatislaong@users.noreply.github.com>
2021-03-03 20:24:09 +0800

    change to capital layout

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,6 @@
 import "./src/styles/global.css";
 import React from "react";
-import Layout from "./src/components/layout";
+import Layout from "./src/components/Layout";
 
 export const wrapPageElement = ({element}) => {
    return <Layout>{element}</Layout>;
