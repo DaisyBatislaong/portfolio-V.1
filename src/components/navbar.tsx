22bc81b89fac504bc482e0936b7b22344d6ac572
Daisy Jane Batislaong <71486565+DaisyBatislaong@users.noreply.github.com>
2021-04-04 16:10:27 +0800

    fixed navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -8,13 +8,13 @@ const Navbar = () => {
    const handleClick = () => {
       setClick(!clicked);
    };
-
+   const hovered = "md:hover:text-yellow-600 md:border-b-2 md:border-transparent md:hover:border-yellow-500"
    return (
       <nav className="w-full">
          <LinkContainer className=" absolute flex flex-col space-y-24 md:space-y-0 md:justify-end md:space-x-16  md:mr-12 md:pt-8 md:flex-row md:relative font-cabin-regular" clicked={clicked} onClick={handleClick}>
-            <Link to="/">Daisy Jane Batislaong</Link>
-            <Link to="/about">About Me</Link>
-            <Link to="/contacts" className="">Contacts</Link>
+            <Link to="/" className={hovered}>Daisy Jane Batislaong</Link>
+            <Link to="/about" className={hovered}>About Me</Link>
+            <Link to="/contacts" className={hovered}>Contacts</Link>
          </LinkContainer>
          <ul className="md:hidden flex justify-end pt-6 mr-6 relative z-50">
             <li onClick={handleClick}>
